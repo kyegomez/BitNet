a3dce94f70bd338f0b4998123998b7e23ed220cb
Kye <kye@apacmediasolutions.com>
2024-03-24 18:51:43 -0700

    [CLEANUP]

--- bitnet/__init__.py
+++ bitnet/__init__.py
@@ -1,11 +1,10 @@
+from bitnet.bit_attention import BitMGQA
 from bitnet.bit_ffn import BitFeedForward
+from bitnet.bit_linear import BitLinearNew
+from bitnet.bit_transformer import BitNetTransformer
+from bitnet.bitlinear import BitLinear
 from bitnet.inference import BitNetInference
 from bitnet.replace_hf import replace_linears_in_hf, replace_linears_in_pytorch_model
-from bitnet.bit_transformer import BitNetTransformer
-from bitnet.bit_attention import BitMGQA
-from bitnet.bit_linear import BitLinearNew
-from bitnet.bitlinear import BitLinear
-
 
 __all__ = [
     "BitFeedForward",
