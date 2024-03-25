f2d0a9eef067393f3284f09674d1900da14adfb9
Kye <kye@apacmediasolutions.com>
2024-03-24 19:36:51 -0700

    [SETUP.PY]

--- setup.py
+++ setup.py
@@ -2,15 +2,18 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name='gemm_lowbit_ext',
+    name='gemm_lowbit_ext',  # Name of your extension
     ext_modules=[
         CUDAExtension(
-            name='gemm_lowbit_ext',
-            sources=['kernel/gemm_lowbit_kernel.cu'],  # Adjusted paths
-            extra_compile_args={'cxx': [], 'nvcc': []}
+            name='gemm_lowbit_ext',  # Extension name, used in Python import
+            sources=['gemm_lowbit_extension.cpp'],  # Source files
+            extra_compile_args={
+                'cxx': [],
+                'nvcc': [],  # You can specify extra args for nvcc here
+            }
         )
     ],
     cmdclass={
-        'build_ext': BuildExtension
+        'build_ext': BuildExtension.with_options(no_python_abi_suffix=True)
     }
 )
