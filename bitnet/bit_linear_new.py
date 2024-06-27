fix: correct sign for new bit linear

bitnet/bit_linear_new.py
@@ -20,8 +20,8 @@ def activation_quant(x: Tensor):
 def weight_quant(w: Tensor):
     scale = w.abs().mean()
     e = w.mean()
-    u = (w - e).sign() * scale
-    return u
+    u = (w - e) * scale
+    return u.sign()
 
 
 class BitLinearNew(nn.Linear):